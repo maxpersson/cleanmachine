d45d128df72c88ebb188a7b25b92c7910333064e
maxpersson <maxpandersen@gmail.com>
2019-08-01 20:30:42 +0200

    integrate distance sensor

---
+++ Assets/control.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control : MonoBehaviour
+{
+
+    public int sharkSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sharkSpeed = gameObject.GetComponent<serialBlow>().distance;
+        Vector3 moving = new Vector3(sharkSpeed, 0, 0);
+        if (sharkSpeed <= 25)
+        {
+            transform.Translate(Vector3.left);
+        }
+        if(sharkSpeed > 25)
+        {
+            transform.Translate(Vector3.right);
+        }
+        
+    }
+}
--- Assets/serialBlow.cs
+++ Assets/serialBlow.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO.Ports;
+using System.Threading;
 using UnityEngine;
 
 public class serialBlow : MonoBehaviour
@@ -15,6 +17,10 @@ public class serialBlow : MonoBehaviour
     float speed;
     float count;
     bool state_check = false;
+    int bytVal1;
+    int bytVal2;
+
+    public int distance;
 
     string infoString;
 
@@ -23,23 +29,23 @@ public class serialBlow : MonoBehaviour
     {
         stream = new SerialPort("COM11", 9600);
         stream.Open();
-
+        // declare thread, and reference sampleFunction
+        Thread sampleThread = new Thread(new ThreadStart(readSerial));
+        sampleThread.IsBackground = true;
+        // start thread
+        sampleThread.Start();
     }
 
     // Update is called once per frame
     void Update()
     {
-        infoString = stream.ReadLine();
-
-        state = float.Parse(infoString);
-
         calculateSpeed();
 
     }
 
     private void OnGUI()
     {
-        GUI.Label(new Rect(10, 10, 300, 100), "lort    " + state.ToString() + "     speed:    " + speed.ToString() + "    time_start:  " + start.ToString() + "     counter:     " + count.ToString()) ;
+        GUI.Label(new Rect(10, 10, 300, 100), "lort    " + state.ToString() + "     speed:    " + speed.ToString() + "    time_start:  " + start.ToString() + "     counter:     " + count.ToString() + "   byte:      " + bytVal1.ToString() + "     distance:" + distance.ToString() );
 
     }
 
@@ -51,25 +57,25 @@ public class serialBlow : MonoBehaviour
             start = Time.time;
         }
 
-            if (state == 0)
+        if (state == 0)
+        {
+            if (!state_check)
             {
-                if (!state_check)
-                {
-                    state_check = true;
-                    count += 1;
-                }
-                
+                state_check = true;
+                count += 1;
+            }
 
-                if (count >= 5)
-                {
+
+            if (count >= 5)
+            {
                 float end = Time.time;
-                timeDifference = (end - start)/1000;
+                timeDifference = (end - start) / 1000;
                 speed = count / timeDifference;
                 end = 0;
                 count = 0;
                 state_check = false;
             }
-            }
+        }
         else
         {
             state_check = false;
@@ -81,5 +87,38 @@ public class serialBlow : MonoBehaviour
     {
         if (stream != null)
             stream.Close();
+    }
+
+    public void readSerial()
+    {
+        while (true)
+        {
+            if (stream == null)
+            {
+                return;
+            }
+            if (stream.IsOpen)
+            {
+                stream.ReadTimeout = 1;
+                try
+                {
+
+                    bytVal1 = stream.ReadByte();
+                    state = bytVal1;
+
+                    bytVal2 = stream.ReadByte();
+                    distance = bytVal2;
+
+                }
+                catch (TimeoutException ex)
+                {
+                    print(ex);
+                }
+            }
+
+
+
+           
+        }
     }
 }
