0ad6e4ee88c064c7c72c3a0c93bacfc309cb4d73
PadawanMiyagi <Jacob@bennedsen.dk>
2019-08-01 15:20:15 +0200

    Added movement scripts

--- Assets/scripts/Gameplay.cs
+++ Assets/scripts/Gameplay.cs
@@ -5,17 +5,31 @@ using UnityEngine;
 public class Gameplay : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject trash1;
+    public GameObject trash2;
+    public GameObject trash3;
+    public GameObject trash4;
+    public GameObject trash5;
+    public GameObject trash6;
+
+    public GameObject[] trashArray;
+
     void Start()
     {
-        GameObject go = new GameObject("New Sprite");
-        SpriteRenderer renderer = go.AddComponent<SpriteRenderer>();
-        print(Resources.Load("Assets 1", typeof(Sprite)));
-        renderer.sprite = Resources.Load("Assets 1", typeof(Sprite)) as Sprite;
+        trashArray = new GameObject[] {trash1, trash2, trash3, trash4, trash5, trash6};
+        createGameObject();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void createGameObject(){
+        GameObject newObject = trashArray[Random.Range(0,5)];
+        newObject.transform.position = new Vector3(-9f,4.2f,0);
+        Instantiate(newObject);
     }
 }
--- Assets/scripts/Trash.cs
+++ Assets/scripts/Trash.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class trashmovement : MonoBehaviour
+public class Trash : MonoBehaviour
 {
+    private float fallSpeed = 0.4f;
+    private float wiggleSpeed = 1.0f;
+    public int count;
+    private float moveVelocity = Random.Range(0,7);
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,26 @@ public class trashmovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        count++;
+        transform.Translate(Vector3.down * fallSpeed * Time.deltaTime, Space.World);
+        if( count%2 == 0){
+            wiggleLeft();
+        }
+        else{wiggleRight();}
+        shootOut();
+    }
+
+    void wiggleLeft(){
+        transform.Translate(Vector3.left * fallSpeed * Time.deltaTime, Space.World);
+    }
+    
+    void wiggleRight(){
+        transform.Translate(Vector3.right * fallSpeed * Time.deltaTime, Space.World);
+    }
+
+    void shootOut(){
+        if(transform.position.y > 1.9){
+            transform.Translate(Vector3.right * moveVelocity * Time.deltaTime, Space.World);
+        }
     }
 }
