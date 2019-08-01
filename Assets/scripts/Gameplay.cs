a60737c34e442c70d89871843213ca0bb82e0c77
PadawanMiyagi <Jacob@bennedsen.dk>
2019-08-01 13:36:54 +0200

    Setup and  sprite script
    Added setup and sprite script

--- Assets/scripts/Gameplay.cs
+++ Assets/scripts/Gameplay.cs
@@ -7,7 +7,10 @@ public class Gameplay : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject go = new GameObject("New Sprite");
+        SpriteRenderer renderer = go.AddComponent<SpriteRenderer>();
+        print(Resources.Load("Assets 1", typeof(Sprite)));
+        renderer.sprite = Resources.Load("Assets 1", typeof(Sprite)) as Sprite;
     }
 
     // Update is called once per frame
